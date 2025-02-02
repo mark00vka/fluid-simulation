5052c5dc4f805ed46be83fde62532ab1f1ff8dc6
Mark Mozarovski <mark.mozh@gmail.com>
2025-02-02 23:54:49 +0100

    add density and pressure

--- main.gd
+++ main.gd
@@ -1,15 +1,29 @@
 class_name Simulation
-extends Node2D
+extends Control
 
-@export var number_of_particles := 100
-@export var number_of_rows := 10
-@export var starting_velocity := Vector2.ZERO
-@export var particle_radius : float = 5.0
+
+const mass = 1
+
+
+@export_category("")
+@export var numberOfParticles : int = 100
+@export var numberOfRows : int = 10
+@export var startingVelocity := Vector2.ZERO
+@export var particleRadius : float = 5.0
+@export var s = 15
+
+@export_category("")
+@export_range(50, 1000, 10) var smoothingRadius : float = 20.0
 @export var separation : float = 5.0
-@export var gravity : float = 200
+@export var gravity : float = 100
 @export var damping : float = 1.0
 
-@onready var viewport_size : Vector2i = DisplayServer.window_get_size()
+@export_category("")
+@export_range(0.01, 0.07, 0.001) var targetDensity : float = 1.0
+@export_range(1, 10, 0.1) var pressureMultiplier : float = 1.0
+@export var densityGradient : Gradient
+
+@onready var viewportSize : Vector2i = DisplayServer.window_get_size()
 
 
 var particles : Array[Particle]
@@ -21,23 +35,113 @@ var always_on_top = true
 func _ready() -> void:
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_ALWAYS_ON_TOP, always_on_top)
 	Particle.simulation = self
-	for i in range(number_of_particles):
-		var x = (i % number_of_rows - number_of_rows / 2) * (particle_radius * 2 + separation)
-		var y = floor(i / number_of_rows - (number_of_particles / number_of_rows / 2)) * (particle_radius * 2 + separation)
+	for i in range(numberOfParticles):
+		var x = (i % numberOfRows - numberOfRows / 2) * (particleRadius * 2 + separation)
+		var y = floor(i / numberOfRows - (numberOfParticles / numberOfRows / 2)) * (particleRadius * 2 + separation)
+		x = randi_range(0, viewportSize.x)
+		y = randi_range(0, viewportSize.y)
 		var pos := Vector2(x, y)
-		particles.append(Particle.new(pos, starting_velocity))
+		particles.append(Particle.new(pos, startingVelocity))
 
 
 func _process(delta: float) -> void:
+	if Engine.get_process_frames() % 100 != 0:
+		pass
+		
 	for i in range(particles.size()):
-		particles[i].update(delta)
+		particles[i].update_densities()
+		
+	for i in range(particles.size()):
+		particles[i].update_velocity(delta)
+		
+	for i in range(particles.size()):
+		particles[i].update_position(delta)
+		
 	queue_redraw()
 
 
 func _draw() -> void:
+	for i in range(viewportSize.x / s + 1):
+		for j in range(viewportSize.y / s + 1):
+			var density = calculate_density(Vector2i(i, j) * s)
+			var densityError = inverse_lerp(targetDensity * 0.5, targetDensity * 1.5, density)
+			var color = sample_gradient(densityError)
+			color.a = 0.3
+			draw_circle(Vector2i(i, j) * s, s, color)
+			
 	for particle in particles:
-		draw_circle(particle.position, particle_radius, Color.WHITE)
+		var densityError = inverse_lerp(targetDensity * 0.5, targetDensity * 1.5, particle.density)
+		draw_circle(particle.position, particleRadius, Color.BLACK)
 		
+		
+func sample_gradient(l : float):
+	return densityGradient.sample(l)
+		
+		
+func smoothing_kernel(radius: float, dst: float):
+	if dst >= radius: return 0
+	var volume = PI * pow(radius, 3) / 6
+	return (radius - dst) * (radius - dst) / volume
+
+	
+func smoothing_kernel_derivative(radius: float, dst: float):
+	if dst >= radius: return 0
+	var s = - 12 / PI / pow(radius, 3)
+	return s * (radius - dst)
+
+
+func calculate_density(pos: Vector2):
+	var density = 0
+	
+	for particle in particles:
+		var dst = pos.distance_to(particle.position)
+		var influence = smoothing_kernel(smoothingRadius, dst)
+		density += mass * influence
+		
+	return density
+	
+	
+func convert_density_to_pressure(density: float):
+	var densityDifference = min(0, targetDensity - density)
+	var pressure = densityDifference * pressureMultiplier * 10000
+	return pressure
+	
+	
+func calculate_property(pos: Vector2):
+	var property = 0
+	
+	for particle in particles:
+		var density = particle.density
+		var dst = pos.distance_to(particle.position)
+		var influence = smoothing_kernel(smoothingRadius, dst)
+		property += particle.property * influence / density
+		
+	return property
+	
+	
+func calculate_shared_pressure(particleA : Particle, particleB : Particle):
+	var pressureA = convert_density_to_pressure(particleA.density)
+	var pressureB = convert_density_to_pressure(particleB.density)
+	
+	return (pressureA + pressureB) / 2
+	
+func calculate_pressure_force(particle: Particle):
+	var pressureForce = Vector2.ZERO
+	
+	for otherParticle in particles:
+		var pos = particle.position
+		
+		if otherParticle.position == pos: continue
+		
+		var dst = pos.distance_to(otherParticle.position)
+		var dir = (otherParticle.position - pos) / dst
+
+		var influence = smoothing_kernel_derivative(smoothingRadius, pos.distance_to(otherParticle.position))
+		var sharedPressure = calculate_shared_pressure(particle, otherParticle)
+		pressureForce += - sharedPressure * dir * mass * influence / otherParticle.density
+		
+	return pressureForce
+	
 	
 func _input(event):
 	if event is InputEventKey and event.pressed and event.keycode == KEY_T:
@@ -52,23 +156,49 @@ func _input(event):
 class Particle:
 	var position: Vector2
 	var velocity: Vector2
+	var property = 1
+	var density = 0
+	
+	
 	static var simulation: Simulation
+	
 	
 	func _init(pos, vel) -> void:
 		position = pos
 		velocity = vel
 	
-	func update(delta : float):
-		velocity.y += simulation.gravity * delta
+	
+	func update_velocity(delta: float):
+		velocity += Vector2.DOWN * simulation.gravity * delta
+		
+		var pressureForce = simulation.calculate_pressure_force(self)
+		var pressureAcceleration = pressureForce / density
+
+		velocity += pressureAcceleration * delta
+	
+	func update_position(delta : float):
 		position += velocity * delta
 		handle_collision()
 		
+		
+	func update_densities():
+		density = simulation.calculate_density(position)
+		
+		
 	func handle_collision():
-		if position.y > simulation.viewport_size.y / 2 - simulation.particle_radius:
+		if position.y > simulation.viewportSize.y - simulation.particleRadius:
 			velocity.y *= -1 * simulation.damping
-			position.y = simulation.viewport_size.y / 2 - simulation.particle_radius
+			position.y = simulation.viewportSize.y - simulation.particleRadius
 			
-		if  position.y < -simulation.viewport_size.y / 2 + simulation.particle_radius:
+		if  position.y < 0 + simulation.particleRadius:
 			velocity.y *= -1 * simulation.damping
-			position.y = -simulation.viewport_size.y / 2 + simulation.particle_radius
+			position.y = 0 + simulation.particleRadius
+	
+		if position.x > simulation.viewportSize.x - simulation.particleRadius:
+			velocity.x *= -1 * simulation.damping
+			position.x = simulation.viewportSize.x - simulation.particleRadius
+			
+		if  position.x < 0 + simulation.particleRadius:
+			velocity.x *= -1 * simulation.damping
+			position.x = 0 + simulation.particleRadius
 	
